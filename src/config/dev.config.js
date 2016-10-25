a1ab3c1a9b6348700f933a52cad3aca1858c6cd5
Joe Carpenito <jcarpenito@sapient.com>
2016-10-25 16:41:40 -0400

    initial dev config file

---
+++ src/config/dev.config.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+
+	EXPRESS_PORT: 1337,
+	
+	Compression: {
+		LEVEL: 9,
+		THRESHOLD: '1b'
+	},
+
+	// TODO: for dev, these should be a login
+	ParticleCredentials: {
+		USER: '',
+		PASS: ''
+	}	
+
+};

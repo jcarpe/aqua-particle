4b26f6d6c35c1919708accb0e3b2e2cb403ebc37
Joe Carpenito <jcarpenito@sapient.com>
2016-10-27 14:15:37 -0400

    add event constants for test lamp status

--- src/constants/particle-events.js
+++ src/constants/particle-events.js
@@ -3,6 +3,8 @@ module.exports = {
 	// test
 	TEST_LAMP_ON: 'aquarium_toggle_on_test_indicator',
 	TEST_LAMP_OFF: 'aquarium_toggle_off_test_indicator',
+	TEST_LAMP_GET_STATUS: 'test_lamp_get_status',
+	TEST_LAMP_POST_STATUS: 'test_lamp_post_status',
 
 	// lights
 	LIGHTS_ON: 'aquarium_on_aqua_lights',
